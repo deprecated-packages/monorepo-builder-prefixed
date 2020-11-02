a5888333535457766178811b91cf2a84f7175251
Tomas Votruba <tomas.vot@gmail.com>
2020-11-02 22:09:34 +0100

    remove gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,31 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0fa4a53ed72d334b9019925c102d956bf2bd2a8b',
+    'name' => 'symplify/monorepo-builder-prefixed',
+  ),
+  'versions' => 
+  array (
+    'symplify/monorepo-builder' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+      ),
+    ),
+    'symplify/monorepo-builder-prefixed' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0fa4a53ed72d334b9019925c102d956bf2bd2a8b',
+    ),
+  ),
+);
